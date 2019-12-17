05123111b04ee51ca07c018ae9eb867e4c549d84
Doug Kintop <dkintop@hotmail.com>
2019-12-17 13:47:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-// Add your code here
+let formData = {
+  dogName: "Byron",
+  dogBreed: "Poodle"
+};
+
+let configObj = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json"
+  },
+  body: JSON.stringify(formData)
+};
+
+function addDog() {
+  fetch("http://localhost:3000/dogs", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      console.log(object);
+    })
+    .catch(function(error) {
+      alert("Bad things! Ragnarők!");
+      console.log(error.message);
+    });
+}
+
+function configObj2(name, email) {
+  return {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      name: `${name}`,
+      email: `${email}`
+    })
+  };
+}
+
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", configObj2(name, email))
+    .then(function(response) {
+      response.json();
+    })
+    .then(function(object) {
+      console.log(object);
+      debugger;
+    })
+    .catch(function(error) {
+      alert("Bad things! Ragnarők!");
+      console.log(error.message);
+    });
+}
