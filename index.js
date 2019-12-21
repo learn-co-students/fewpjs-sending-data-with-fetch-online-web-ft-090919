879ce16bb4309b2e0b5ef2977e030ec51339b995
Kenneth Spatta <kenyounot@gmail.com>
2019-12-21 16:16:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your code here
+
+function submitData(name, email) {
+    
+    let formData = {
+        name: name,
+        email: email
+    };
+
+   let configObj = {
+       method: "POST",
+       headers: {
+           "Content-Type": "application/json",
+           "Accept": 'application/json'
+       },
+       body: JSON.stringify(formData)
+   }
+
+   return fetch('http://localhost:3000/users', configObj)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object){
+            let body = document.getElementsByTagName('body')[0];
+            let h1 = document.createElement('h1');
+            h1.innerHTML = object.id;
+            body.appendChild(h1);
+        })
+        .catch(function(error){
+            let body = document.getElementsByTagName('body')[0];
+            let h1 = document.createElement('h1');
+
+            h1.innerHTML = error.message;
+            body.appendChild(h1);
+        })
+};
