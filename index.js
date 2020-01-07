25269690c7fcf931acfba500259ce288d882655b
DBuckley0126 <DannyBoy.0126>
2020-01-07 02:35:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+
+function submitData(name, email){
+  const configObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  }
+
+  return fetch("http://localhost:3000/users", configObject)
+  .then(response => {return response.json()})
+  .then(object => {
+    // let element = document.createElement("p")
+    // element.innerText = (object.id)
+    // document.body.appendChild(element)
+    document.body.innerHTML = object.id
+  }).catch(message => {
+    console.log(message)
+    document.body.innerHTML = message
+  })
+}
