5bcfbb30ec4c630cd1b31fd55689d0f7de22e0f1
mplichta <bigcatplichta@gmail.com>
2020-01-06 20:27:49 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(name, email) {
+    let formData = {
+        name: `${name}`,
+        email: `${email}`
+      };
+    
+    let configObj = {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+       
+    return fetch('http://localhost:3000/users', configObj)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            document.body.innerHTML = object.id;
+        })
+        .catch(function(error) {
+            alert("Most of this code was copy/pasted from the lesson, this should work!");
+            document.body.innerHTML = error.message;
+        });
+        
+}
