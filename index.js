98f4bd3ba40d34f1ec88480c6ac182066f169bff
Matthew Ream <matthewtimothyream@gmail.com>
2020-01-08 20:15:27 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+
+function submitData(name, email) {
+    let formData = {
+        name: name,
+        email: email
+      };
+       
+      let configObj = {
+          method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+      };
+       
+      fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(object) {
+          document.body.innerHTML = object.id
+          console.log(object.id)
+        })
+        .catch(function(error) {
+          alert("Bad things! Ragnarők!");
+          console.log(error.message);
+        });
+}
